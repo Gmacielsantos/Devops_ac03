ce0338199591709f96c388af9ca7de15f5877ae5
Gmacielsantos <70041035+Gmacielsantos@users.noreply.github.com>
2020-10-09 18:21:52 -0300

    Update and rename com/maciel/test/test_operacoes.py to com/maciel/teste/test_operacoes.py

--- com/maciel/test/test_operacoes.py
+++
@@ -1,4 +0,0 @@
-def media_OPE(M1,M2,M3,M4,M5,M6,M7):
-	return (M1+M2+M3+M4+M5+M6+M7)/7
-def test_Media_OPE():
-	assert media_OPE_Final(1400,1400,2800,1414,714,1421,721) == 1410
---
+++ com/maciel/teste/test_operacoes.py
@@ -0,0 +1,2 @@
+def teste_mortes_covid(F1,F2,F3,F4):
+	return (F1+F2+F3+F4)/4
